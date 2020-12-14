adbaafee5f560237410ed66b2eb934a7a556b73c
Nazar-Kostyk <kostyknazar1@gmail.com>
2020-12-14 18:04:15 +0200

    Substitution of forbidden strings to empty strings error was corrected

--- module_1/bonus/bonus_task.rb
+++ module_1/bonus/bonus_task.rb
@@ -1,7 +1,7 @@
 def compare_strings(str1, str2)
-  # change value of given strings if they contain other characters than letters or are nil
-  str1 = '' unless str1.match?(/\A[a-zA-Z]*\z/) || str1
-  str2 = '' unless str2.match?(/\A[a-zA-Z]*\z/) || str2
+  # change value of given strings if they are nils or contain forbidden characters
+  str1 = '' unless str1 && str1.match?(/\A[a-zA-Z]*\z/)
+  str2 = '' unless str2 && str2.match?(/\A[a-zA-Z]*\z/)
 
   # calculate and compare sums of ASCII codes
   str1.upcase.bytes.sum == str2.upcase.bytes.sum
